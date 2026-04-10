d266f8e42cb85cdf96f99d0d2fd91ed6c7a19c71
Mark-Arboo <170411749+mark-arboo@users.noreply.github.com>
2026-04-10 16:10:13 +0200

    Add files via upload

---
+++ asset/menu.js
@@ -0,0 +1,61 @@
+let menuInfo = [
+{
+    "icon": "fa-home",
+    "text_key": "home",
+    "link": "showWelcomePanel()"
+},
+{
+    "icon": "fa-key",
+    "text_key": "checkin_out",
+    "link": "showCheckinOutPanel()"
+},
+{
+    "icon": "fa-wifi",
+    "text_key": "wifi",
+    "link": "showWifiPanel()"
+},
+{
+    "icon": "fa-list-ol",
+    "text_key": "house_rules",
+    "link": "showHouseRulesPanel()"
+},
+{
+    "icon": "fa-cutlery",
+    "text_key": "restaurant",
+    "link": "showRestaurantPanel()"
+},
+
+/*
+{
+    "icon": "fa-bed",
+    "text_key": "rooms",
+    "link": "rooms.html"
+},
+{
+    "icon": "fa-map",
+    "text_key": "location",
+    "link": "location.html"
+},
+{
+    "icon": "fa-calendar",
+    "text_key": "booking",
+    "link": "booking.html"
+},
+{
+    "icon": "fa-map",
+    "text_key": "location",
+    "link": "location.html"
+},
+{
+    "icon": "fa-info",
+    "text_key": "info",
+    "link": "info.html"
+},
+{
+    "icon": "fa-phone",
+    "text_key": "contacts",
+    "link": "contacts.html"
+}
+*/
+
+];
