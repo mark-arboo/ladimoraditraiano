f4e955cd2e22bc6894e7a880d09c0faa8caac170
Mark-Arboo <170411749+mark-arboo@users.noreply.github.com>
2026-06-20 12:37:16 +0200

    Fix immagini restaurant e cafe

--- asset/cafe.js
+++ asset/cafe.js
@@ -1,62 +1,62 @@
 const cafeObj = [
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAGE8qR-zkbkbXBgGzVnQyyboZSOl3b-fBiWdql91ISPu0PqpK5uhyauUFPO8YVU6gqI4wpn67vfRWnwV1haneRPlsnkrjEt2toNUEPlecxdVfPQzr0s1mxkRo--s2InVKdSs8WWoomeyQ7m=w408-h532-k-no",
+        "image": "./img/cafe/nando.png",
         "name": "CinciNando Lounge Bar",
         "map": "https://www.google.it/maps/place/CinciNando+Lounge+Bar/@41.2868701,13.2540329,17z/data=!3m1!4b1!4m6!3m5!1s0x132523664cd8a01b:0x3bf94cb389b191b8!8m2!3d41.2868701!4d13.2540329!16s%2Fg%2F11rms4klmt?authuser=0&hl=it&entry=ttu&g_ep=EgoyMDI2MDUxMi4wIKXMDSoASAFQAw%3D%3D",
         "street": "Via Traiano",
         "tel": "+39 339 2202865"    
     },
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAFdj990iHJU54vETjcSV5EfBenHCE8wIP3lcVA7trcK-mbPV_XkOd4RA7LF9aOkpTZpuDXQlPPzU6b3zLQuW2RkMLAs9Fwu-N-Bi9E6B8nvpjGgIXytk1-dNknXiesNslMIlHKr=w408-h544-k-no",
+        "image": "./img/cafe/royalcafe.png",
         "name": "Royal Caffè di Palmacci Franco",
         "map": "https://www.google.it/maps/place/Royal+Caff%C3%A8+di+Palmacci+Franco/@41.2890285,13.2519127,17z/data=!3m1!4b1!4m6!3m5!1s0x132523684b2aacd1:0xd01748d9162362c1!8m2!3d41.2890285!4d13.2519127!16s%2Fg%2F1pv1g2mzl?authuser=0&hl=it&entry=ttu&g_ep=EgoyMDI2MDUxMi4wIKXMDSoASAFQAw%3D%3D",
         "street": "Via Roma, 88/90",
         "tel": "+39 0773 368932"    
     },
     {
-        "image": "https://streetviewpixels-pa.googleapis.com/v1/thumbnail?panoid=zfDobj_Xs2uNdD94dsxaNw&cb_client=search.gws-prod.gps&w=408&h=240&yaw=214.66058&pitch=0&thumbfov=100",
+        "image": "./img/cafe/cavour11.png",
         "name": "Cavour 11 caffetteria & Cocktail bar",
         "map": "https://www.google.it/maps/place/Cavour+11+caffetteria+%26+Cocktail+bar/@41.2873637,13.2560881,17z/data=!3m1!4b1!4m6!3m5!1s0x1325231453e5fb8b:0x5bb62fa57d6604b8!8m2!3d41.2873637!4d13.2560881!16s%2Fg%2F11wqv9jvyn?authuser=0&hl=it&entry=ttu&g_ep=EgoyMDI2MDUxMi4wIKXMDSoASAFQAw%3D%3D",
         "street": "Via Camillo Benso Conte di Cavour, 11",
         "tel": "+39 349 3064301"    
     },
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAFu5nBpB0zX1OIwFAsuDogEMgMTqSJF9tmRPi2xT6YOjOTx0YhiAlP1qfhN3NqdmTQexqg-ZHtOKGnj-xvhEpQHNASRidBvVfy1UCCREZPT0n0cAri_nzPMNFrH_UZ5IWYBS6M58A=w408-h544-k-no",
+        "image": "./img/cafe/pupo.png",
         "name": "Bar Pupo",
         "map": "https://www.google.it/maps/place/Bar+Pupo/@41.2873556,13.2561157,17z/data=!3m1!4b1!4m6!3m5!1s0x13252367cca139b7:0x93c29ef6a931d3e!8m2!3d41.2873556!4d13.2561157!16s%2Fg%2F11ckqr7kb3?authuser=0&hl=it&entry=ttu&g_ep=EgoyMDI2MDUxMi4wIKXMDSoASAFQAw%3D%3D",
         "street": "Via Camillo Benso Conte di Cavour, 16",
         "tel": "+39 0773 703863"    
     },
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAFEEUzzPmSz9KblDqCpmp_CC5u_iQrpucu09oK8njGlbqfEPvVbDW6vDlKiqlVRKCXSPkNvUJhlrFrRqhnh0Dpl2TBEUyCHEH1Psr7aMMPjZ6eApYc2tOYbj4gzzgJgyll8ijryLHdms_i9=w426-h240-k-no",
+        "image": "./img/cafe/jamm.png",
         "name": "Jamm",
         "map": "https://www.google.it/maps/place/Jamm/@41.2881485,13.2499808,17z/data=!3m1!4b1!4m6!3m5!1s0x13252327908c3ca3:0x2865aa83ffa49619!8m2!3d41.2881485!4d13.2499808!16s%2Fg%2F11n3zzszpm?authuser=0&hl=it&entry=ttu&g_ep=EgoyMDI2MDUxMi4wIKXMDSoASAFQAw%3D%3D",
         "street": "Viale della Vittoria, 11",
         "tel": "+39 0773 061377"    
     },
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAGoih3hFZm7VXOzTjxkz1Ymj8qgnjaCGJJh1TRve1UA6olrXrbUQj6XU_L0Hb8hZK8_iVm8HzvyWjgsHw_HUj19vbEQdIcujqPXjZ4Mxy9kz1TH2_CD4HIQXSFYzpFZBo7IMHTO=w426-h240-k-no",
+        "image": "./img/cafe/barpineta.png",
         "name": "Bar Della Pineta",
         "map": "https://www.google.it/maps/place/Bar+Della+Pineta/@41.284068,13.2523948,17z/data=!3m1!4b1!4m6!3m5!1s0x1325236f18e3fb11:0x5b081239a980075f!8m2!3d41.284068!4d13.2523948!16s%2Fg%2F11clvbh5gx?authuser=0&hl=it&entry=ttu&g_ep=EgoyMDI2MDUxMi4wIKXMDSoASAFQAw%3D%3D",
         "street": "Via Cristoforo Colombo, 48",
         "tel": "+39 0773 726734"    
     },
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAG_A96RvzoZeCaGUI6uGmSL2nAYgDfP8mkfDBPl1hBdZtF7YJVXYfIHT6tYUJLsaLWYLGSf-jHC37lxyfSF31TSSe68iShId-1yt-3lQkeOz07xLEUmzF2BY7451w-f2FBtIHZE=w408-h305-k-no",
+        "image": "./img/cafe/duomo.png",
         "name": "Caffè del Duomo",
         "map": "https://www.google.it/maps/place/Caff%C3%A8+del+Duomo/@41.2916269,13.2485531,17z/data=!3m1!4b1!4m6!3m5!1s0x13252341f1411e5d:0x2b197d2d443c2cf9!8m2!3d41.2916269!4d13.2485531!16s%2Fg%2F1tl5_lkm?authuser=0&hl=it&entry=ttu&g_ep=EgoyMDI2MDUxMi4wIKXMDSoASAFQAw%3D%3D",
         "street": "Piazza Municipio, 17",
         "tel": "+39 0773 709528"    
     },
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAG3LuSvxRteV1gRX4697a7zS_2r_Scq78ZLJ9qAzasEmAO3xzvnA4tzPkVRSWUOJ7UgYeomefXY6d-Jz7z85r0Wzhau4BG5UqFMl18f2soC3AdnLPLs_GyapdmUuAoqhSfPWOQp8A=w408-h544-k-no",
+        "image": "./img/cafe/somma.png",
         "name": "Caffè Somma",
         "map": "https://www.google.it/maps/place/Caff%C3%A8+Somma/@41.2885766,13.2498349,17z/data=!3m1!4b1!4m6!3m5!1s0x132523699d53ec2f:0xfe6ab4ff5d3363bb!8m2!3d41.2885766!4d13.2498349!16s%2Fg%2F11bysjq2p_?authuser=0&hl=it&entry=ttu&g_ep=EgoyMDI2MDUxMi4wIKXMDSoASAFQAw%3D%3D",
         "street": "Viale della Vittoria, 12",
         "tel": "+39 0773 726377"    
     },
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAEgMxQTIMPHA1BzCZIx_WObQOW21D0nIEHQu65ISsJcggOxIfymabKU83rURtKHh2VSS2ntyrrIRCD_8UMWvxPZiiKwsT6hlfL0RTGv_H7k2NGsHovTQuXP_dwqjRjl7QHmVmca=w408-h246-k-no",
+        "image": "./img/cafe/delrio.png",
         "name": "Bar-Pasticceria del Rio",
         "map": "https://www.google.it/maps/place/Bar-Pasticceria+del+Rio/@41.2885371,13.2520385,17z/data=!3m1!4b1!4m6!3m5!1s0x132523684d4f5c9f:0x1cc1feea8ebcaa77!8m2!3d41.2885371!4d13.2520385!16s%2Fg%2F11fxzsccft?authuser=0&hl=it&entry=ttu&g_ep=EgoyMDI2MDUxMi4wIKXMDSoASAFQAw%3D%3D",
         "street": "Via del Rio, 16",
--- asset/restaurant.js
+++ asset/restaurant.js
@@ -1,55 +1,55 @@
 const restaurantObj = [
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAEpq7V11Bu0Zzdd7us0jxReiK_5t92lp0iMvGEnLfhIjQ3X8cfjFW6p2VUhC_xUDbdNE2mbJcGZlovHiFmsMPhXXR5gKMIXlfh8nlMrjPRDD6K56sdA6M3SZx66mrocMNnaoRrF=w408-h272-k-no",
+        "image": "./img/restaurant/vicoletto.png",
         "name": "Hostaria del Vicoletto",
         "map": "https://www.google.it/maps/place/Hostaria+del+Vicoletto/data=!4m7!3m6!1s0x1325236626c0e53f:0x7ae9d08fa2fc4b37!8m2!3d41.2866667!4d13.255!16s%2Fg%2F1v5drvct!19sChIJP-XAJmYjJRMRN0v8oo_Q6Xo?authuser=0&hl=it&rclk=1",
         "street": "Via del Quartiere, 9",
         "tel": "+39 0773 703781"    
     },
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAGsYqLLJUemhgFk1xYyWnv47wJh4Oz4eAERooxNET8KxMaoHHWx3qzmmurw8fWyJlAu-pR-Zz80MlCPENfyqWiFKVMn2refgjwP6GmqDI2sALdkkAgPuDPg00juPuMZ6YvDU_8t=w427-h240-k-no",
+        "image": "./img/restaurant/laspasella.png",
         "name": "Hostaria La Spasella 2.0",
         "map": "https://www.google.it/maps/place/Hostaria+La+Spasella+2.0/data=!4m7!3m6!1s0x132523e370dd1569:0xe6f9e86e3a541e85!8m2!3d41.288296!4d13.2506233!16s%2Fg%2F11nx1ht6b2!19sChIJaRXdcOMjJRMRhR5UOm7o-eY?authuser=0&hl=it&rclk=1",
         "street": "Via Lungolinea Pio VI",
         "tel": "+39 0773 59126"    
     },
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAEEnWW0sFV7dDxYgYtss1SOHNnVD_HmqXiYBkaVw2TQlHi0J7PNj_lfN-nbKlqdAEIVHPgiB95XVY8WuyQm9TbCJB-mqoTEIXsQPn33W3nnOnJF6w3UlogRP5hLfRLJAJhpQTiqQHcRy-FC=w408-h306-k-no",
+        "image": "./img/restaurant/ostematto.png",
         "name": "L'Oste Matto",
         "map": "https://www.google.it/maps/place/L%E2%80%99Oste+Matto/data=!4m7!3m6!1s0x132523232cd41ec1:0x8cee934a08fc3cee!8m2!3d41.2921306!4d13.2480667!16s%2Fg%2F11f130wjzg!19sChIJwR7ULCMjJRMR7jz8CEqT7ow?authuser=0&hl=it&rclk=1",
         "street": "Corso Anita Garibaldi, 98-100",
         "tel": "+39 348 350 0910"    
     },
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAFOlYOWWbArHvXBBkAh_gPYJ8_RoI8QADkltBsGibEKvaBK9XgAxhMJSPoR3D6T1xsFwEei0CTyiAVuSkEqFiM5kTs6AX5lZIFZ1LW9uffnqOdfjLSpOeolsmvrToc20I5oFkyz=w426-h240-k-no",
+        "image": "./img/restaurant/ilgranchio.png",
         "name": "Il Granchio",
         "map": "https://www.google.it/maps/place/Il+Granchio/@41.2912292,13.2470598,17z/data=!4m17!1m9!2m8!1sRistoranti!3m5!2sVia+Traiano!3s0x132523689f011981:0x18e99287f92a0a3d!4m2!1d13.2549574!2d41.2864409!6e5!3m6!1s0x4067b678308c2595:0x3c38ad170f2e9f16!8m2!3d41.2912292!4d13.2518234!15sCgpSaXN0b3JhbnRpWgwiCnJpc3RvcmFudGmSAQpyZXN0YXVyYW50mgEkQ2hkRFNVaE5NRzluUzBWSlEwRm5TVU5VZDI5NUxUWm5SUkFC4AEA-gEECAAQRg!16s%2Fg%2F11b6c67wh6?hl=it&entry=ttu&g_ep=EgoyMDI2MDUwNi4wIKXMDSoASAFQAw%3D%3D",
         "street": "Via S. Francesco Nuovo, 80",
         "tel": "+39 0773 709696"    
     },
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAEAMnFpQbMR-jlw9zTjFQ5K4bdXNfYEPkV9h40mdLp-8Fs7i2AHFHLZoZWGbWZHIYhKqubbrqBikoav91CwBc6SdFE80mK6JAAq2py0owd2X9LbP9ojC02UQph2r5bZ9xkWk_BSZg=w408-h306-k-no",
+        "image": "./img/restaurant/centroittico.png",
         "name": "Centro Ittico da Ernesto",
         "map": "https://www.google.it/maps/place/Centro+Ittico+da+Ernesto/@41.2845026,13.2431119,17z/data=!4m17!1m9!2m8!1sRistoranti!3m5!2sVia+Traiano!3s0x132523689f011981:0x18e99287f92a0a3d!4m2!1d13.2549574!2d41.2864409!6e5!3m6!1s0x1325236c5a549771:0xa449549b36eed15e!8m2!3d41.2845026!4d13.2474893!15sCgpSaXN0b3JhbnRpWgwiCnJpc3RvcmFudGmSAQpyZXN0YXVyYW50mgEjQ2haRFNVaE5NRzluUzBWSlEwRm5TVVJtZGtsRExVeEJFQUXgAQD6AQQIABBH!16s%2Fg%2F11bzs3pk3l?hl=it&entry=ttu&g_ep=EgoyMDI2MDUwNi4wIKXMDSoASAFQAw%3D%3D",
         "street": "Piazzale Lido",
         "tel": "+39 0773 723043"    
     },
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAEvAkzF6CAbl-okJKyO3yFFZ2DVA8wbhEPg0eEnDgUK_4X1PDme1lx2l4XIUENIvRBWoRc_kvk5TcAr_efjKqLVBSRiVS_fsmYJbTE_kCipg6HRGY0kArtSNqr5lGU3UUVxF9S03XBXYew=w408-h544-k-no",
+        "image": "./img/restaurant/tavernabafio.png",
         "name": "La Taverna di Bafio",
         "map": "https://www.google.it/maps/place/La+Taverna+di+Bafio/@41.2886308,13.2477082,17z/data=!4m17!1m9!2m8!1sRistoranti!3m5!2sVia+Traiano!3s0x132523689f011981:0x18e99287f92a0a3d!4m2!1d13.2549574!2d41.2864409!6e5!3m6!1s0x13252300796a3b23:0xe44d9214e27da07a!8m2!3d41.2883794!4d13.2525176!15sCgpSaXN0b3JhbnRpWgwiCnJpc3RvcmFudGmSAQpyZXN0YXVyYW50mgFEQ2k5RFFVbFJRVU52WkVOb2RIbGpSamx2VDI1T1JrMXBNVlprVlZad1pWZEtTbVZ0Y0c1YVJuQXpVVmN4VEZKSVl4QULgAQD6AQQIERA6!16s%2Fg%2F11x690vx_h?hl=it&entry=ttu&g_ep=EgoyMDI2MDUwNi4wIKXMDSoASAFQAw%3D%3D",
         "street": "Piazza Giuseppe Garibaldi, 17",
         "tel": "+39 328 2640913"    
     },
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAET1Jshjh-MLCFe4oEJTmLWnnNgGl48XJcPZ59WNxKXMNOswR5A32eARblz48t6tBCMZBuC6hV1XEbtPkhZXfwhTTDniwKKbM66lpHaHpDB_g94bjKv4EwT0QvmEBUfG0qLDhA=w408-h306-k-no",
+        "image": "./img/restaurant/locandaruggieri.png",
         "name": "Locanda Ruggieri",
         "map": "https://www.google.it/maps/place/Locanda+Ruggieri/@41.2886308,13.2477082,17z/data=!3m1!5s0x132523684d5d16b7:0x302a897cd0eecd43!4m17!1m9!2m8!1sRistoranti!3m5!2sVia+Traiano!3s0x132523689f011981:0x18e99287f92a0a3d!4m2!1d13.2549574!2d41.2864409!6e5!3m6!1s0x132523a962e831bf:0xcbbd641de2b7e811!8m2!3d41.2886308!4d13.2520856!15sCgpSaXN0b3JhbnRpWgwiCnJpc3RvcmFudGmSAQpyZXN0YXVyYW50mgEjQ2haRFNVaE5NRzluUzBWSlEwRm5TVU5hTm5aTGNVOW5FQUXgAQD6AQQIABAz!16s%2Fg%2F11h42rk3x8?hl=it&entry=ttu&g_ep=EgoyMDI2MDUwNi4wIKXMDSoASAFQAw%3D%3D",
         "street": "Via del Rio, 10",
         "tel": "+39 333 4758507"    
     },
     {
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAHuRq3e59cOqRuFT7fPX0w7-5XVNnpCwDBqA762QLm5CmX7lQDoszhD4cOkEFZ-eTkO2NFItysqosBJN7zOh99P8U6ZPrYLkglg4raDSwXtLn6XcMViGUN4HP-lXdkpCQvNPL1M=w408-h408-k-no",
+        "image": "./img/restaurant/anticadogana.png",
         "name": "Antica Dogana",
         "map": "https://www.google.it/maps/place/Antica+Dogana/@41.2873579,13.2541956,18z/data=!4m15!1m8!3m7!1s0x132523689f011981:0x18e99287f92a0a3d!2sVia+Traiano,+04019+Terracina+LT!3b1!8m2!3d41.2864409!4d13.2549574!16s%2Fg%2F1tg0h3tq!3m5!1s0x1325236709937413:0xe09559a85c5e80d6!8m2!3d41.2880707!4d13.2582388!16s%2Fg%2F1ptxqbcjw?hl=it&entry=ttu&g_ep=EgoyMDI2MDUwNi4wIKXMDSoASAFQAw%3D%3D",
         "street": "Via G. Marconi, 21",
